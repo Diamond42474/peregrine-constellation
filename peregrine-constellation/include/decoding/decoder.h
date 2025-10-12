8fff029cbe3bb78a25515aea8db7279ceac0216d
Logan Miller <loganmiller040402@gmail.com>
2025-10-11 22:21:11 -0700

    Implemented decoder header

--- peregrine-constellation/include/decoding/decoder.h
+++ peregrine-constellation/include/decoding/decoder.h
@@ -1,37 +1,49 @@
 #ifndef DECODER_H
 #define DECODER_H
 
-typedef enum {
+#include "fsk_decoder.h"
+#include "byte_assembler.h"
+#include "cobs_decoder.h"
+
+typedef enum
+{
     MESSAGE_DECODER_NONE,
     MESSAGE_DECODER_COBS,
 } message_decoder_e;
 
-typedef enum {
+typedef enum
+{
     BYTE_DECODER_NONE,
     BYTE_DECODER_BIT_STUFFING,
 } byte_decoder_e;
 
-typedef enum {
+typedef enum
+{
     BIT_DECODER_NONE,
     BIT_DECODER_FSK,
 } bit_decoder_e;
 
-typedef struct {
-    message_decoder_e message_decoder;
-    byte_decoder_e byte_decoder;
+typedef struct
+{
     bit_decoder_e bit_decoder;
+    byte_decoder_e byte_decoder;
+    message_decoder_e message_decoder;
+
+    void *bit_decoder_handle;
+    void *byte_decoder_handle;
+    void *message_decoder_handle;
 } decoder_handle_t;
 
 int decoder_init(decoder_handle_t *handle);
 int decoder_deinit(decoder_handle_t *handle);
 
-int decoder_set_message_decoder(decoder_handle_t *handle, message_decoder_e type);
-message_decoder_e decoder_get_message_decoder(decoder_handle_t *handle);
-int decoder_set_byte_decoder(decoder_handle_t *handle, byte_decoder_e type);
-byte_decoder_e decoder_get_byte_decoder(decoder_handle_t *handle);
-int decoder_set_bit_decoder(decoder_handle_t *handle, bit_decoder_e type);
-bit_decoder_e decoder_get_bit_decoder(decoder_handle_t *handle);
+int decoder_set_message_decoder(decoder_handle_t *handle, message_decoder_e type, void *message_decoder_handle);
+int decoder_set_byte_decoder(decoder_handle_t *handle, byte_decoder_e type, void *byte_decoder_handle);
+int decoder_set_bit_decoder(decoder_handle_t *handle, bit_decoder_e type, void *bit_decoder_handle);
 
+int decoder_task(decoder_handle_t *handle);
+int decoder_process_samples(decoder_handle_t *handle, const uint16_t *samples, size_t num_samples);
 
-
+bool decoder_has_message(decoder_handle_t *handle);
+int decoder_get_message(decoder_handle_t *handle, unsigned char *buffer, size_t buffer_len, size_t *message_len);
 #endif // DECODER_H
---
+++ peregrine-constellation/src/decoding/decoder.c
@@ -0,0 +1,7 @@
+#include "decoder.h"
+
+#include "c-logger.h"
+#include "fsk_decoder.h"
+#include "byte_assembler.h"
+#include "cobs_decoder.h"
+
