da8670c2cf0cd3e31f856028978f4b4678040583
Logan Miller <loganmiller040402@gmail.com>
2026-06-06 15:01:50 -0700

    refactor: Reduced PTT delay

--- Core/Inc/interface/pconfig.h
+++ Core/Inc/interface/pconfig.h
@@ -28,6 +28,6 @@
 
 // Modem
 #define pconfigMODEM_TX_BUFFER_SIZE (pconfigMAX_PAYLOAD_SIZE * 2) // Buffer for outgoing data to be transmitted, should be multiple of max payload size
-#define pconfigPTT_DELAY_MS (500)                                 // Delay between setting PTT high and starting transmission to allow hardware to stabilize
+#define pconfigPTT_DELAY_MS (250)                                 // Delay between setting PTT high and starting transmission to allow hardware to stabilize
 
 #endif // pconfig_H
